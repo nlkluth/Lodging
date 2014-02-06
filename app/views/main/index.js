7364d9f4ee001644575432e3c4e9db9223556d8e
Nathan Kluth <nlkluth@gmail.com>
2014-02-06 12:35:22 -0700

    setting view hierarchy

--- app/views/main/index.js
+++ app/views/main/index.js
@@ -1,8 +1,13 @@
-var View, template;
+var View, template, Locations;
 
 View = require('../view').View;
 template = require('./templates/index');
+Locations = require('../location');
 
 module.exports = View.extend({
-  template: template
+  template: template,
+
+  initialize: function() {
+    this.setView('.locations', new Locations(), true);
+  }
 });
