04f92862dd4dfec10453e348ba0a43d9b9e00cfd
Nathan Kluth <nlkluth@gmail.com>
2014-02-07 12:01:24 -0700

    setting date field with defaults

--- app/models/search.js
+++ app/models/search.js
@@ -2,4 +2,9 @@ var Model;
 
 Model = require('./model');
 
-module.exports = Model.extend({});
+module.exports = Model.extend({
+  initialize: function() {
+    this.set('arrivalDate', new Date());
+    this.set('departureDate', new Date());
+  }
+});
